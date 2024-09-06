3a169805312c7aa43011c2d114416a02dedb605f
Malgorzata Jaskiewicz <malgorzatahannajaskiewicz@gmail.com>
2024-09-06 11:06:18 +0100

    add simply pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Ruter, Route, Redirect, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
+import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace';
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+  <Ruter>
+    <Switch>
+    <Route path="/" exact>
+    <Users />
+    </Route>
+    <Route path="/places/new" exact>
+    <NewPlace />
+    </Route>
+    <Redirect to="/"/>
+    </Switch>
+  </Ruter>
+  );
 }
 
 export default App;
