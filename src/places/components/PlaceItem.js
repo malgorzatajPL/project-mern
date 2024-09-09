34aade6d57a06c419f264f4b5a6c82839fc6852d
Malgorzata Jaskiewicz <malgorzatahannajaskiewicz@gmail.com>
2024-09-09 12:26:31 +0100

    add place list adn placeholder for places

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,25 +1,28 @@
-import React from "react"
-import Card from "../../shared/components/UIElements/Card"
+import React from "react";
+import Card from "../../shared/components/UIElements/Card";
+import "./PlaceItem.css";
 
-import "./PlaceItem.css"
-
-const PlaceItem = props => {
-    return <li className="place-item">
-        <Card className="place-item__content">
+const PlaceItem = (props) => {
+    console.log(props)
+  return (
+    <li className="place-item">
+      <Card className="place-item__content">
         <div className="place-item__image">
-            <img scr={props.image} alt={props.title} />
+          <img src={props.image} alt={props.title} />
         </div>
         <div className="place-item__info">
-            <h2>{props.title}</h2>
-            <h3>{props.address}</h3>
-            <p>{props.description}</p>
+          <h2>{props.title}</h2>
+          <h3>{props.address}</h3>
+          <p>{props.description}</p>
         </div>
         <div className="place-item__actions">
-            <button>VIEW ON MAP</button>
-            <button>EDIT</button>
-            <button>DELETE</button>
+          <button>VIEW ON MAP</button>
+          <button>EDIT</button>
+          <button>DELETE</button>
         </div>
-        </Card>
+      </Card>
     </li>
-}
-export default PlaceItem
+  );
+};
+
+export default PlaceItem;
